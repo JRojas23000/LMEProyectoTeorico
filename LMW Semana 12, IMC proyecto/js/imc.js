01c51e5ba5eb0e860eb8cd34758b15fb3f8740f6
JRojas23000 <143213967+JRojas23000@users.noreply.github.com>
2023-10-25 20:34:31 -0600

    Cambio 25/10
    Cambio de diseño CSS y mensaje de respuesta en JS

--- LMW Semana 12, IMC proyecto/js/imc.js
+++ LMW Semana 12, IMC proyecto/js/imc.js
@@ -12,21 +12,21 @@ function calcular() {
     document.getElementById("result").innerHTML=imc.toFixed(2);
 
     if (imc < 16) {
-		grado = "Delgadez severa.";
+		grado = "Delgadez severa.Veáse imagen 1 para referencia.";
 	} else if (imc >= 16 && imc <= 16.99) {
-		grado = "Delgadez moderada.";
+		grado = "Delgadez moderada. Veáse imagen 1 para referencia.";
 	} else if (imc >= 17 && imc <= 18.49) {
 		grado = "Delgadez aceptable.";
 	} else if (imc >= 18.5 && imc <= 24.99) {
-		grado = "Normal.";
+		grado = "Normal. Veáse imagen 1 para referencia.";
 	} else if (imc >= 25 && imc <= 29.99) {
-		grado = "Preobeso.";
+		grado = "Preobeso. Veáse imagen 1 para referencia.";
 	} else if (imc >= 30 && imc <= 34.99) {
-		grado = "Obeso tipo I.";
+		grado = "Obeso tipo I. Veáse imagen 1 para referencia.";
 	} else if (imc >= 35 && imc <= 39.99) {
-		grado = "Obeso tipo II.";
+		grado = "Obeso tipo II. Veáse imagen 1 para referencia.";
 	} else if (imc > 40 ) {
-		grado = "Obeso tipo III.";
+		grado = "Obeso tipo III. Veáse imagen 1 para referencia.";
 	}
     document.getElementById("aceon").innerHTML=grado;
 
